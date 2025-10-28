9d39e5d598d847caf9050cdd4596189638aad4d6
HII LIPSA <lipsabarik108@gmail.com>
2025-10-28 16:56:28 +0530

    htmlcode

---
+++ arrys.js
@@ -0,0 +1,7 @@
+let number=[45,55,124,46,58]
+let sum=0
+for(let i=0;i<number.length;i++){
+    sum+= number[i];
+
+}
+console.log("sum of arry ="+sum);
---
+++ sum1task1.js
@@ -0,0 +1,18 @@
+function arraySum(arr, index =0){
+    if(index >=arr.length){
+        return 0;
+    }
+
+return arr[index]+arraySum(arr,index+1);
+}
+
+
+// alterntive
+function arraySum(arr){
+
+}
+
+
+
+
+
